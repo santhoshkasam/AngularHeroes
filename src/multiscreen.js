ecd1089c7d74a2b62091655302d3672b16e919e8
Kasam <santhosh.kasam@thomsonreuters.com>
2018-07-19 15:56:04 +0530

    Added 3 screens and tour

--- src/multiscreen.js
+++ src/multiscreen.js
@@ -1,4 +1,15 @@
-var tour = new Shepherd.Tour({
+  // wait ms milliseconds
+  function wait(ms) {
+    return new Promise(r => setTimeout(r, ms));
+  }
+
+  async function hello() {
+    alert('function call');
+    await wait(5000);
+  }
+
+
+  var tour = new Shepherd.Tour({
     defaults: {
       classes: 'shepherd-theme-arrows',
       //buttons: false,
@@ -7,10 +18,10 @@ var tour = new Shepherd.Tour({
     }
   });
 
-  tour.addStep('FirstStep', {
-    title: 'First Name',
-    text: 'Please enter the <b>First Name</b> here',
-    attachTo: '#Firsttextbox bottom',
+  tour.addStep('Dashboard', {
+    title: 'Hero',
+    text: 'Click on the tile see the details',
+    attachTo: '.module bottom',
     classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
     buttons: [
       {
@@ -18,8 +29,14 @@ var tour = new Shepherd.Tour({
         classes: 'shepherd-button-example-primary',
         action: function()
         {
+          goHome();
           tour.next();
-        },
+        } ,
+        // events: {
+        //   click: function () {
+        //   return  window.location.href='/heroes';
+        //   }
+        // }
       },
       {
         text: 'End Tour',
@@ -28,10 +45,10 @@ var tour = new Shepherd.Tour({
       }
     ]
   });
-  tour.addStep('SecondStep', {
-    title: 'Second Name',
-    text: 'Please enter the <b>Second Name</b> here',
-    attachTo: '#Secondtextbox right',
+  tour.addStep('HeroList', {
+    title: 'Hero Name',
+    text: 'Click here to see the hero derails',
+    attachTo: '.badge right',
     classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
     buttons: [
       {
@@ -50,30 +67,51 @@ var tour = new Shepherd.Tour({
       }
     ]
   });
-  tour.addStep('ThirdStep', {
+  tour.addStep('EditHeroDetail', {
+    title: 'Hero Name',
+    text: 'Edit the Hero Name here',
+    attachTo: '#lblEditName right',
+    classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
+    buttons: [
+      {
+        text: 'Back',
+        classes: 'shepherd-button-example-primary',
+        action: tour.back
+      }, {
+        text: 'Next',
+        action: tour.next,
+        classes: 'shepherd-button-example-primary'
+      },
+      {
+        text: 'End Tour',
+        classes: 'shepherd-button-example-primary',
+        action: tour.cancel
+      }
+    ]
+  });
+  tour.addStep('GoBack', {
     title: 'Last Name',
-    text: 'Please enter the <b>Last Name</b> here',
-    attachTo: '#Thirdtextbox bottom',
+    text: 'Click here to go back to previous screen',
+    attachTo: '#btnGoback bottom',
     classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
     buttons: [
       {
         text: 'Back',
-        classes: 'shepherd-button-example-primary',
-        action: tour.back
-      }, {
-        text: 'Next',
-        action: tour.next,
+        action: tour.back,
         classes: 'shepherd-button-example-primary'
-      },
-      {
-        text: 'End Tour',
-        classes: 'shepherd-button-example-primary',
-        action: tour.cancel
+      }, {
+        text: 'Exit',
+        classes: 'shepherd-button-secondary',
+        action: function () {
+          return tour.hide();
+        }
       }
     ]
   });
-function callnextstep()
-{
-    Shepherd.activeTour.next();
-}
   tour.start();
+
+  function call() {
+    //if (First.isOpen()) {
+    Shepherd.activeTour.next();
+    //}
+  };
